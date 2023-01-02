8359f680543cf5d10ca9b49ea06e66b0f3495cc9
ahmetbibi <ahmetbibi@gmail.com>
2023-01-02 22:52:04 +0100

    all cruds implemented for product repository class

--- src/Services/Catalog/Catalog.API/Repositories/ProductRepository.cs
+++ src/Services/Catalog/Catalog.API/Repositories/ProductRepository.cs
@@ -8,44 +8,64 @@ namespace Catalog.API.Repositories
     {
         private readonly ICatalogContext _context;
 
+        // Dependeency Injection
         public ProductRepository(ICatalogContext context)
         {
             _context = context ?? throw new ArgumentNullException(nameof(context));
         }
 
-        public Task CreateProduct(Product product)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<bool> DeleteProduct(string id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<Product> GetProduct(string id)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<IEnumerable<Product>> GetProductByCategory(string categoryName)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task<IEnumerable<Product>> GetProductByName(string name)
-        {
-            throw new NotImplementedException();
-        }
-
         public async Task<IEnumerable<Product>> GetProducts() => await _context
                                                                            .Products
-                                                                           .Find(prop => true)
+                                                                           .Find(p => true)
                                                                            .ToListAsync();
 
-        public Task<bool> UpdateProduct(Product product)
+        public async Task<Product> GetProduct(string id) => await _context
+                                                                        .Products
+                                                                        .Find(p => p.Id == id)
+                                                                        .FirstOrDefaultAsync();
+
+        public async Task<IEnumerable<Product>> GetProductByName(string name)
         {
-            throw new NotImplementedException();
+            FilterDefinition<Product> filter = Builders<Product>.Filter.Eq(p => p.Name, name);
+
+            return await _context
+                .Products
+                .Find(filter)
+                .ToListAsync();
         }
+
+        public async Task<IEnumerable<Product>> GetProductByCategory(string categoryName)
+        {
+            FilterDefinition<Product> filter = Builders<Product>.Filter.Eq(p => p.Category, categoryName);
+
+            return await _context
+                .Products
+                .Find(filter)
+                .ToListAsync();
+        }
+
+        public async Task CreateProduct(Product product) => await _context.Products.InsertOneAsync(product);
+
+        public async Task<bool> UpdateProduct(Product product)
+        {
+            var updaeResult = await _context
+                    .Products
+                    .ReplaceOneAsync(filter: g => g.Id == product.Id, replacement: product);
+
+            return updaeResult.IsAcknowledged && updaeResult.ModifiedCount > 0;
+        }
+
+        public async Task<bool> DeleteProduct(string id)
+        {
+            FilterDefinition<Product> filter = Builders<Product>.Filter.Eq(p => p.Id, id);
+
+            DeleteResult deleteResult = await _context
+                                                    .Products
+                                                    .DeleteOneAsync(filter);
+
+            return deleteResult.IsAcknowledged && deleteResult.DeletedCount > 0;
+        }
+        
+       
     }
 }
